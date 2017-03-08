8385d021f0ba8079b85a677827def93fe56b8654
Thomas <tjohannes.ee@gmail.com>
2017-03-08 04:40:37 -0800

    Update callback_01.js

--- callback/callback_01.js
+++ callback/callback_01.js
@@ -1,1 +1,22 @@
 
+let add = function(a, b) { // anonymous function
+    sum = ""
+    for (let i = a; i <= b; i++) {
+        sum = a + b
+    }
+    return sum;
+};
+
+let subtract = function(a, b) {
+    return a - b;
+};
+let multiply = function(a, b) {
+    return a * b;
+};
+let divide = function(a, b) {
+    return Math.floor(a / b);
+};
+function cal(num1, num2, callback) {
+    return callback(num1, num2);
+}
+console.log(cal(3, 3, add));
